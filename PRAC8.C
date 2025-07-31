2b894348f4ef1e8d42edef867a370b0732c0288e
manthan01ce <manthan.ce01@gmail.com>
2025-07-31 16:04:08 +0530

    Add files via upload

---
+++ PRAC8.C
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int age;
+char ch;
+printf("Enter your age :");
+X: scanf(" %d", age);
+(age> 18) ? printf("eligible") : printf("not eligible");
+
+scanf("%c", &ch);
+if (ch == 'y')
+{
+ goto X;
+}
+
+getch();
+
+}
+
+
+
